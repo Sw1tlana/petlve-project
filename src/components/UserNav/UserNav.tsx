7da638c3954f9c05f0d0a509a8aa71cf26f93fc6
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2025-03-14 23:05:51 +0200

    changes button, fonts

--- src/components/UserNav/UserNav.tsx
+++ src/components/UserNav/UserNav.tsx
@@ -1,9 +1,12 @@
-import '../../scss/components/btn/types/_primary.scss';
+import '../../../src/scss/components/btn/types/_primary.scss';
+import '../../../src/scss/components/btn/types/_secondary.scss';
 
 function UserNav() {
   return (
     <div>
+      <h2>Hello</h2>
         <button className="btn btn--primary" type="button">Log out</button>
+        <input className="input input--secondary" type='search' placeholder='input'/>
     </div>
   )
 };
