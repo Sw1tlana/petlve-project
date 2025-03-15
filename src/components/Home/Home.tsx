8f221a6ef6e46525160690d594d53a5efece8319
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2025-03-15 12:07:05 +0200

    add fonts, brecpoints mixin, body style, main text

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,19 @@
+import Container from "../../shared/components/Container/Container";
+
+function Home() {
+  return (
+      <Container>
+        <section>
+          <div>
+
+          </div>
+          <div>
+            
+          </div>
+          
+        </section>
+      </Container>
+  )
+};
+
+export default Home;
--- src/components/UserNav/UserNav.tsx
+++ src/components/UserNav/UserNav.tsx
@@ -4,9 +4,7 @@ import '../../../src/scss/components/btn/types/_secondary.scss';
 function UserNav() {
   return (
     <div>
-      <h2>Hello</h2>
         <button className="btn btn--primary" type="button">Log out</button>
-        <input className="input input--secondary" type='search' placeholder='input'/>
     </div>
   )
 };
