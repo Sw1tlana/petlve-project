cda8a280363f70735e37fbbd9d9901adabc65251
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2025-03-30 21:18:24 +0300

    add Loader

--- src/shared/components/Loader.tsx/Loader.tsx
+++ src/shared/components/Loader.tsx/Loader.tsx
@@ -1,46 +1,46 @@
 import style from '../../../scss/components/_loader.module.scss';
-import { useState, useEffect } from "react";
-import Container from '../Container/Container';
+import { useState, useEffect, useRef } from "react";
 
 function Loader() {
     const [progress, setProgress] = useState(0);
+    const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
     useEffect(() => {
-      const interval = setInterval(() => {
+      intervalRef.current = setInterval(() => {
         setProgress((prev) => {
           if(prev >= 100) {
-            clearInterval(interval);
-
             return 100;
           }
 
           return prev + 1;
         })
       }, 50);
-      return () => clearInterval(interval);
+      
+      return () => {
+        if (intervalRef.current) clearInterval(intervalRef.current); 
+    };
+
     }, []);
 
   return (
-    <>
-      <Container>
-      <div className={style.loaderContainer}>
-                        <svg width="100" height="100" viewBox="0 0 100 100" className={style.loaderSvg}>
-                    <circle cx="50" cy="50" r="40" stroke="#ddd" strokeWidth="5" fill="none" />
-                    <circle
-                        cx="50" cy="50" 
-                        r="40" 
-                        stroke="#007bff" 
-                        strokeWidth="5"
-                        fill="none"
-                        strokeDasharray="251.2"
-                        strokeDashoffset={251.2 - (progress / 100) * 251.2}
-                        strokeLinecap="round"
-                    />
-                    <text x="50" y="55" textAnchor="middle" fontSize="16" fill="#007bff">{progress}%</text>
-                </svg>
-      </div>
-      </Container>
-    </>
+    <div className={style.loaderContainer}> 
+    <svg width="100" height="100" viewBox="0 0 100 100" className={style.loaderSvg}>
+        <circle cx="50" cy="50" r="40" stroke="#ddd" strokeWidth="5" fill="none" />
+        <circle
+            cx="50" cy="50"
+            r="40"
+            stroke="#ffffff"
+            strokeWidth="5"
+            fill="none"
+            strokeDasharray="251.2"
+            strokeDashoffset={251.2 - (progress / 100) * 251.2}
+            strokeLinecap="round"
+        />
+        <text x="50" y="55" textAnchor="middle" fontSize="16" fill="#ffffff">
+            {progress}%
+        </text>
+    </svg>
+</div>
   )
 };
 
